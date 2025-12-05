cb2adac74fb8a09a5c966fb37862948585f76997
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-05 20:54:42 +0300

    feat: add the class Product

--- src/main/java/com/example/productmanagement/model/Product.java
+++ src/main/java/com/example/productmanagement/model/Product.java
@@ -1,10 +1,11 @@
-package org.example.productmanagement.model;
+package com.example.productmanagement.model;
 
 import java.time.Instant;
 
 public class Product {
     private int id;
     private String name;
+    private double price;
     private Instant creationDatetime;
     private Category category;
 
@@ -13,6 +14,50 @@ public class Product {
         this.name = name;
         this.creationDatetime = creationDatetime;
         this.category = category;
+    }
+
+    public Product() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Instant getCreationDatetime() {
+        return creationDatetime;
+    }
+    public void setCreationDatetime(Instant creationDatetime) {
+        this.creationDatetime = creationDatetime;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", category=" + category + "]";
     }
 
     private String getCategoryName() {
