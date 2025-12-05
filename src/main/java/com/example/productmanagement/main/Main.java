309957a77a8f6223856971929ca7307a30f23448
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-05 20:57:48 +0300

    feat: add Main class

--- src/main/java/com/example/productmanagement/main/Main.java
+++ src/main/java/com/example/productmanagement/main/Main.java
@@ -1,4 +1,13 @@
 package com.example.productmanagement.main;
 
+import com.example.productmanagement.service.DataRetriever;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        DataRetriever dataRetriever = new DataRetriever();
+        System.out.println(dataRetriever.getAllCategories());
+        System.out.println(dataRetriever.getProductList(2, 3));
+    }
 }
