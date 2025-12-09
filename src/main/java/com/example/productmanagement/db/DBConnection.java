0a3869f75b7d4d49647273d53350ba44cdae2297
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-09 16:56:15 +0300

    fix: make the method and attribut non static

--- src/main/java/com/example/productmanagement/db/DBConnection.java
+++ src/main/java/com/example/productmanagement/db/DBConnection.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
-    private static final String USER = "product_manager_user";
-    private static final String PASSWORD = "123456";
+    private final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private final String USER = "product_manager_user";
+    private final String PASSWORD = "123456";
 
-    public static Connection getDBConnection() throws SQLException {
+    public Connection getDBConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
