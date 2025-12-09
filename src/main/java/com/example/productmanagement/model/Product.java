869c6dc177883ed06c2a4628b2754fd843a480c2
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-09 16:55:13 +0300

    fix the constructor of product

--- src/main/java/com/example/productmanagement/model/Product.java
+++ src/main/java/com/example/productmanagement/model/Product.java
@@ -9,9 +9,10 @@ public class Product {
     private Instant creationDatetime;
     private Category category;
 
-    public Product(int id, String name, Instant creationDatetime, Category category) {
+    public Product(int id, String name, double price, Instant creationDatetime, Category category) {
         this.id = id;
         this.name = name;
+        this.price = price;
         this.creationDatetime = creationDatetime;
         this.category = category;
     }
