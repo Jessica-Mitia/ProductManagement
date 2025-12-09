b4d5dc9e6955e36e809741a93e189e8f1ccc502e
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-09 19:00:09 +0300

    feat: add test

--- src/test/java/DataRetrieverTest.java
+++ src/test/java/DataRetrieverTest.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+import com.example.productmanagement.db.DBConnection;
+import com.example.productmanagement.model.Category;
+import com.example.productmanagement.model.Product;
+import com.example.productmanagement.service.DataRetriever;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DataRetrieverTest {
+    @Test
+    void testDBConnection() throws SQLException {
+        DBConnection db = new DBConnection();
+        Connection con = db.getDBConnection();
+        assertNotNull(con);
+        assertFalse(con.isClosed());
+    }
+
+    @Test
+    void getAllCategoriesTest() throws SQLException {
+        DataRetriever dataRetriever = new DataRetriever();
+        List<Category> expected = new ArrayList<>();
+        expected.add(new Category(1, "Informatique"));
+        expected.add(new Category(2, "Téléphonie"));
+        expected.add(new Category(3, "Audio"));
+        expected.add(new Category(4, "Accessoires"));
+        expected.add(new Category(5,"Informatique"));
+        expected.add(new Category(6, "Bureau"));
+        expected.add(new Category(7, "Mobile"));
+
+        List<Category> categories = dataRetriever.getAllCategories();
+
+        assertEquals(expected, categories);
+    }
+
+    @Test
+    void  getProductListTest() throws SQLException {
+        DataRetriever dataRetriever = new DataRetriever();
+
+        List<Product> products = dataRetriever.getProductList(1, 4);
+        assertEquals(4, products.size());
+        assertEquals("Laptop Dell XPS", products.get(0).getName());
+
+        List<Product> products2 = dataRetriever.getProductList(2, 3);
+        assertEquals(3, products2.size());
+        assertEquals("Clavier Logitech", products2.get(0).getName());
+    }
+
+
 }
